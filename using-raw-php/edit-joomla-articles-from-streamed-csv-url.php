a0953029545e19e4020087153991654d9bf04561
Mr Alexandre J-S William ELISÉ <code@apiadept.com>
2023-03-30 12:16:50 -0400

    Edit original author email

--- using-raw-php/edit-joomla-articles-from-streamed-csv-url.php
+++ using-raw-php/edit-joomla-articles-from-streamed-csv-url.php
@@ -4,7 +4,7 @@ declare(strict_types=1);
 /**
  * Edit Joomla! articles from streamed csv url (with PATCH)
  *
- * @author        Alexandre ELISÉ <contact@alexandree.io>
+ * @author        Alexandre ELISÉ <code@apiadept.com>
  * @copyright (c) 2009 - present. Alexandre ELISÉ. All rights reserved.
  * @license       GPL-2.0-and-later GNU General Public License v2.0 or later
  * @link          https://alexandree.io
