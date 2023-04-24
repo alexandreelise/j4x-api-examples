db2bc3619faf1b5a86f2a3fb3a884ac6d3ec7cb7
Mr Alexandre J-S William ELISÉ <code@apiadept.com>
2023-04-24 16:34:34 -0400

    Add convenient utilities (#17)

--- using-raw-php/smart-add-edit-to-multiple-sites-from-csv-url.php
+++ using-raw-php/smart-add-edit-to-multiple-sites-from-csv-url.php
@@ -44,7 +44,14 @@ $customFieldKeys = []; //['with-coffee','with-dessert','extra-water-bottle'];
 $silent = 1;
 
 // Line numbers we want in any order (e.g 9,7-7,2-4,10,17-14,21). Leave empty '' to process all lines (beginning at line 2. Same as csv file)
-$whatLineNumbersYouWant = '2-30';
+$whatLineNumbersYouWant = '';
+
+defined('IS_CLI') || define('IS_CLI', PHP_SAPI == 'cli' ? true : false);
+defined('CUSTOM_LINE_END') || define('CUSTOM_LINE_END', PHP_SAPI == 'cli' ? PHP_EOL . '===================' . PHP_EOL : '<br>===================<br>');
+defined('ANSI_COLOR_RED') || define('ANSI_COLOR_RED', PHP_SAPI == 'cli' ? "\033[31m" : '');
+defined('ANSI_COLOR_GREEN') || define('ANSI_COLOR_GREEN', PHP_SAPI == 'cli' ? "\033[32m" : '');
+defined('ANSI_COLOR_BLUE') || define('ANSI_COLOR_BLUE', PHP_SAPI == 'cli' ? "\033[34m" : '');
+defined('ANSI_COLOR_NORMAL') || define('ANSI_COLOR_NORMAL', PHP_SAPI == 'cli' ? "\033[0m" : '');
 
 $computedLineNumbers = function (string $wantedLineNumbers = '') {
     if ($wantedLineNumbers === '') {
@@ -99,15 +106,15 @@ $nested = function (array $arr, int $isSilent = 0): array {
     $iterator = new RecursiveIteratorIterator(new RecursiveArrayIterator($arr), RecursiveIteratorIterator::CATCH_GET_CHILD);
     foreach ($iterator as $key => $value) {
         if (mb_strpos($value, '{') === 0) {
-            echo $isSilent == 2 ? sprintf("\033[34m item with key: %s with value: %s\033[0m%s", $key, $value, PHP_EOL) : '';
+            echo $isSilent == 2 ? sprintf("%s item with key: %s with value: %s%s%s", ANSI_COLOR_BLUE, $key, $value, ANSI_COLOR_NORMAL, CUSTOM_LINE_END) : '';
             // Doesn't seem to make sense at first but this one line allows to show intro/fulltext images and urla,urlb,urlc
             $handleComplexValues[$key] = json_decode(str_replace(["\n", "\r", "\t"], '', trim($value)));
         } elseif (json_decode($value) === false) {
             $handleComplexValues[$key] = json_encode($value);
-            echo $isSilent == 2 ? sprintf("\033[34m item with key: %s with value: %s\033[0m%s", $key, $value, PHP_EOL) : '';
+            echo $isSilent == 2 ? sprintf("%s item with key: %s with value: %s%s%s", ANSI_COLOR_BLUE, $key, $value, ANSI_COLOR_NORMAL, CUSTOM_LINE_END) : '';
         } else {
             $handleComplexValues[$key] = $value;
-            echo $isSilent == 2 ? sprintf("\033[34m item with key: %s with value: %s\033[0m%s", $key, $value, PHP_EOL) : '';
+            echo $isSilent == 2 ? sprintf("%s item with key: %s with value: %s%s%s", ANSI_COLOR_BLUE, $key, $value, ANSI_COLOR_NORMAL, CUSTOM_LINE_END) : '';
         }
     }
 
@@ -244,64 +251,70 @@ $process = function (string $givenHttpVerb, string $endpoint, string $dataString
 };
 $expandedLineNumbers = $computedLineNumbers($whatLineNumbersYouWant);
 $storage = [];
+try {
+    $csvReader($csvUrl, $customFieldKeys, $nested, $silent, $expandedLineNumbers, function ($dataValue) use (&$storage, $endpoint, $baseUrl, $basePath, $token, $silent, $timeout, $process) {
 
-$csvReader($csvUrl, $customFieldKeys, $nested, $silent, $expandedLineNumbers, function ($dataValue) use (&$storage, $endpoint, $baseUrl, $basePath, $token, $silent, $timeout, $process) {
+        $dataCurrentCSVline = $dataValue['line'];
+        $dataString = $dataValue['content'];
 
-    $dataCurrentCSVline = $dataValue['line'];
-    $dataString = $dataValue['content'];
+        $curl = curl_init();
 
-    $curl = curl_init();
+        try {
+            if (!is_string($dataString)) {
+                return;
+            }
 
-    try {
-        if (!is_string($dataString)) {
+            $decodedDataString = json_decode($dataString, false, 512, JSON_THROW_ON_ERROR);
+            if ($decodedDataString === false) {
+                return;
+            }
+
+            if (!($token[$decodedDataString->tokenindex] ?? false)) {
+                return;
+            }
+
+
+            // HTTP request headers
+            $headers = [
+                'Accept: application/vnd.api+json',
+                'Content-Type: application/json',
+                'Content-Length: ' . mb_strlen($dataString),
+                sprintf('X-Joomla-Token: %s', trim($token[$decodedDataString->tokenindex])),
+            ];
+
+            // Article primary key. Usually 'id'
+            $pk = (int)$decodedDataString->id;
+
+            $output = $process($pk ? 'PATCH' : 'POST', $endpoint($baseUrl[$decodedDataString->tokenindex], $basePath, $pk), $dataString, $headers, $timeout, $curl);
+            $decodedJsonOutput = json_decode($output, false, 512, JSON_THROW_ON_ERROR);
+
+            // don't show errors, handle them gracefully
+            if (isset($decodedJsonOutput->errors) && (!($storage[$dataCurrentCSVline] ?? false))) {
+                // If article is potentially a duplicate (already exists with same alias)
+                $storage[$dataCurrentCSVline] = ['mightExists' => $decodedJsonOutput->errors[0]->code === 400, 'decodedDataString' => $decodedDataString,];
+                return;
+            }
+            echo $silent == 1 ? sprintf("%s Deployed to: %s, CSV Line: %d, type: %s, id: %d, title: %s, alias: %s, created: %s%s%s",
+                ANSI_COLOR_GREEN,
+                $decodedDataString->tokenindex,
+                $dataCurrentCSVline,
+                $decodedJsonOutput->data->type,
+                $decodedJsonOutput->data->id,
+                $decodedJsonOutput->data->attributes->title,
+                $decodedJsonOutput->data->attributes->alias,
+                $decodedJsonOutput->data->attributes->created,
+                ANSI_COLOR_NORMAL, CUSTOM_LINE_END) : '';
+        } catch (Throwable $e) {
+            echo $silent == 1 ? sprintf("%s Error message: %s, Error code line: %d, Error CSV Line: %d%s%s", ANSI_COLOR_RED, $e->getMessage(), $e->getLine(), $dataCurrentCSVline, ANSI_COLOR_NORMAL, CUSTOM_LINE_END) : '';
             return;
+        } finally {
+            curl_close($curl);
         }
+    });// Execute the function
+} catch (Throwable $e) {
+    echo $silent == 1 ? sprintf("%s Error message: %s, Error code line: %d, %s%s", ANSI_COLOR_RED, $e->getMessage(), $e->getLine(), ANSI_COLOR_NORMAL, CUSTOM_LINE_END) : '';
+}
 
-        $decodedDataString = json_decode($dataString, false, 512, JSON_THROW_ON_ERROR);
-        if ($decodedDataString === false) {
-            return;
-        }
-
-        if (!($token[$decodedDataString->tokenindex] ?? false)) {
-            return;
-        }
-
-        // HTTP request headers
-        $headers = [
-            'Accept: application/vnd.api+json',
-            'Content-Type: application/json',
-            'Content-Length: ' . mb_strlen($dataString),
-            sprintf('X-Joomla-Token: %s', trim($token[$decodedDataString->tokenindex])),
-        ];
-
-        // Article primary key. Usually 'id'
-        $pk = (int)$decodedDataString->id;
-
-        $output = $process($pk ? 'PATCH' : 'POST', $endpoint($baseUrl[$decodedDataString->tokenindex], $basePath, $pk), $dataString, $headers, $timeout, $curl);
-        $decodedJsonOutput = json_decode($output, false, 512, JSON_THROW_ON_ERROR);
-
-        // don't show errors, handle them gracefully
-        if (isset($decodedJsonOutput->errors) && (!($storage[$dataCurrentCSVline] ?? false))) {
-            // If article is potentially a duplicate (already exists with same alias)
-            $storage[$dataCurrentCSVline] = ['mightExists' => $decodedJsonOutput->errors[0]->code === 400, 'decodedDataString' => $decodedDataString,];
-            return;
-        }
-        echo $silent == 1 ? sprintf("\033[32m Deployed to: %s, CSV Line: %d, type: %s, id: %d, title: %s, alias: %s, created: %s\033[0m%s",
-            $decodedDataString->tokenindex,
-            $dataCurrentCSVline,
-            $decodedJsonOutput->data->type,
-            $decodedJsonOutput->data->id,
-            $decodedJsonOutput->data->attributes->title,
-            $decodedJsonOutput->data->attributes->alias,
-            $decodedJsonOutput->data->attributes->created,
-            PHP_EOL) : '';
-    } catch (Throwable $e) {
-        echo $silent == 1 ? sprintf("\033[31m Error message: %s, Error code line: %d, Error CSV Line: %d\033[0m%s", $e->getMessage(), $e->getLine(), $dataCurrentCSVline, PHP_EOL) : '';
-        return;
-    } finally {
-        curl_close($curl);
-    }
-});// Execute the function
 
 // Handle errors and retries
 foreach ($storage as $dataCurrentCSVlineToRetry => $item) {
@@ -330,7 +343,8 @@ foreach ($storage as $dataCurrentCSVlineToRetry => $item) {
             ];
             $output = $process($pk ? 'PATCH' : 'POST', $endpoint($baseUrl[$item['decodedDataString']->tokenindex], $basePath, $pk), $dataString, $headers, $timeout, $curlRetry);
             $result = json_decode($output, false, 512, JSON_THROW_ON_ERROR);
-            echo $silent == 1 ? sprintf("\033[32m Deployed to: %s, CSV Line: %d, type: %s, id: %d, title: %s, alias: %s, created: %s\033[0m%s",
+            echo $silent == 1 ? sprintf("%s Deployed to: %s, CSV Line: %d, type: %s, id: %d, title: %s, alias: %s, created: %s%s%s",
+                ANSI_COLOR_GREEN,
                 $item['decodedDataString']->tokenindex,
                 $dataCurrentCSVlineToRetry,
                 $result->data->type,
@@ -338,10 +352,10 @@ foreach ($storage as $dataCurrentCSVlineToRetry => $item) {
                 $result->data->attributes->title,
                 $result->data->attributes->alias,
                 $result->data->attributes->created,
-                PHP_EOL) : '';
+                ANSI_COLOR_NORMAL, CUSTOM_LINE_END) : '';
         }
     } catch (Throwable $e) {
-        echo $silent == 1 ? sprintf("\033[31m Error message: %s, Error code line: %d, Error CSV Line: %d\033[0m%s", $e->getMessage(), $e->getLine(), $dataCurrentCSVlineToRetry, PHP_EOL) : '';
+        echo $silent == 1 ? sprintf("%s Error message: %s, Error code line: %d, Error CSV Line: %d%s%s", ANSI_COLOR_RED, $e->getMessage(), $e->getLine(), $dataCurrentCSVlineToRetry, ANSI_COLOR_NORMAL, CUSTOM_LINE_END) : '';
         continue;
     } finally {
         curl_close($curlRetry);
